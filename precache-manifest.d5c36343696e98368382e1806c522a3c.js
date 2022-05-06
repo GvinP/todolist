19ef9f24159569451bb9cf90f6a18d3351789884
GvinP <gvinpin72@gmail.com>
2022-05-06 16:30:13 +0400

    Updates

---
+++ precache-manifest.d5c36343696e98368382e1806c522a3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fae2adc962fd1c7a29030892af7b8900",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "a70c6f243ba4b132d316",
+    "url": "/ToDoList/static/css/main.d780d2b8.chunk.css"
+  },
+  {
+    "revision": "9ab703ade68efbdea016",
+    "url": "/ToDoList/static/js/2.64d8814f.chunk.js"
+  },
+  {
+    "revision": "cd7dcd1184256679828870d2be65aca9",
+    "url": "/ToDoList/static/js/2.64d8814f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a70c6f243ba4b132d316",
+    "url": "/ToDoList/static/js/main.25fa6c5c.chunk.js"
+  },
+  {
+    "revision": "448f1ac8991118963dc3",
+    "url": "/ToDoList/static/js/runtime-main.97949b04.js"
+  }
+]);
